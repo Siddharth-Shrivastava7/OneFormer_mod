3f811fb8ad3558d9d9a68ade62edcf881c028368
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-06-14 17:22:21 +0530

    update

--- demo/demo.py
+++ demo/demo.py
@@ -52,11 +52,22 @@ def setup_cfg(args):
     cfg.freeze()
     return cfg
 
+
+## adopted from dannet repo << 
+palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153, 153, 153, 153, 250, 170, 30,
+           220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60, 255, 0, 0, 0, 0, 142, 0, 0, 70,
+           0, 60, 100, 0, 80, 100, 0, 0, 230, 119, 11, 32]
+zero_pad = 256 * 3 - len(palette)
+for i in range(zero_pad):
+    palette.append(0)
+
+
 def put_pallete_save_labelmap(labelmap, png_filename):
     output = Image.fromarray(labelmap.astype(np.uint8)).convert('P')
-    palette = np.zeros((len(CSLabels.id2label), 3), dtype=np.uint8)
-    for label_id, label in CSLabels.id2label.items():
-        palette[label_id] = label.color
+    ## adopted from DDP repo <<
+    # palette = np.zeros((len(CSLabels.id2label), 3), dtype=np.uint8)
+    # for label_id, label in CSLabels.id2label.items():
+    #     palette[label_id] = label.color
     output.putpalette(palette)
     output.save(png_filename)
     return 
